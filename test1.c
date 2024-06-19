1fd626748bce27891f557c01945e6e6ddebb6607
ubuntu <ubuntu@ubuntu.myguest.virtualbox.org>
2024-06-19 13:38:54 -0300

    segredo

--- test1.c
+++ test1.c
@@ -2,7 +2,7 @@
 
 int main(void)
 {
-	printf("teste2");
+	printf("teste3");
 	int i = 1;
 	char aws_access_key_id[] = "AKIAQYLPMN5HHHFPZAM2";
 	char aws_secret_access_key[] = "tUm636uS1yOEcfP5pvfqJ/ml36mF7AkyHsEU0IU";
