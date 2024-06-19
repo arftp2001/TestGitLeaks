88d24238627e1d51a4109194f0afc750b09c449b
arftp2001 <169170489+arftp2001@users.noreply.github.com>
2024-06-19 13:36:54 -0300

    Update test1.c
    add secret in code

--- test1.c
+++ test1.c
@@ -4,5 +4,7 @@ int main(void)
 {
 	printf("teste2");
 	int i = 1;
+	char aws_access_key_id[] = "AKIAQYLPMN5HHHFPZAM2";
+	char aws_secret_access_key[] = "tUm636uS1yOEcfP5pvfqJ/ml36mF7AkyHsEU0IU";
 	return 0;
 }
